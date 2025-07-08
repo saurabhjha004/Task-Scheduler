2720618db4f422cb8a3e50db18c1f099b10666c5
Saurabh Jha <146720857+saurabhjha004@users.noreply.github.com>
2025-07-08 15:57:26 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,8 @@
-require("./passport");
 require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const authModel = require("./Models/Model");
-const bcrypt = require("bcrypt");
-const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const passport = require("passport");
-const TodoRoutes = require("./Routes/TodoRoutes");
-const NoteRoutes = require("./Routes/NoteRoutes");
 const TaskRoutes = require("./Routes/TaskRoutes");
 
 const PORT = 8080;
@@ -42,104 +34,6 @@ app.use(
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get("/", (req, res) => {
-  res.json(" hello ");
-});
-
-app.post("/register", async (req, res) => {
-  const { userName, email, password } = req.body;
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const newAuth = new authModel({
-    userName: userName,
-    email: email,
-    password: hashedPassword,
-  });
-
-  try {
-    const user = await authModel.findOne({ email: email });
-    if (user) res.json("Already Registerd");
-    else {
-      const savedUser = await newAuth.save();
-      res.send(savedUser);
-    }
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-//Google authentication using passport
-
-app.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: process.env.FRONTEND_DOMAIN,
-    successRedirect: `${process.env.FRONTEND_DOMAIN}/Home`,
-  })
-);
-
-//For Facebook Authentication
-
-app.get("/facebook", passport.authenticate("facebook", { scope: ["email"] }));
-
-app.get(
-  "/facebook/callback",
-  passport.authenticate("facebook", {
-    failureRedirect: process.env.FRONTEND_DOMAIN,
-    successRedirect: `${process.env.FRONTEND_DOMAIN}/Home`,
-  })
-);
-
-//Local Login
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: process.env.FRONTEND_DOMAIN,
-  }),
-  (req, res) => {
-    res.json({ success: "successfully logged in" });
-  }
-);
-
-//logout
-app.get("/logout", (req, res, next) => {
-  req.logOut((err) => {
-    if (err) res.send(err);
-    else res.json({ success: "logged out" });
-  });
-});
-
-app.get("/getUser", (req, res, next) => {
-  if (req.user) {
-    res.json(req.user);
-  }
-});
-
-//Forgot and reset password
-app.post("/resetPassword/:id/:token", async (req, res) => {
-  const { id, token } = req.params;
-  console.log(id);
-  const { newPassword } = req.body;
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, encode) => {
-    if (err) return res.send({ Status: "Try again after few minutes" });
-    else {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(newPassword, salt);
-      authModel
-        .findByIdAndUpdate({ _id: id }, { password: hashedPassword })
-        .then((u) => res.send({ Status: "success" }))
-        .catch((err) => res.send({ Status: err }));
-    }
-  });
-});
 
 app.post("/forgotpass", async (req, res) => {
   const { email } = req.body;
